22944ec11ac448788fe0713acc96ede8d4be4bc6
Annasophiathorsgaard <athorsgaard@icloud.com>
2025-02-18 14:02:38 +0100

    added id

--- js/product.js
+++ js/product.js
@@ -1,17 +1,18 @@
+const myId = new URLSearchParams(window.location.search).get("id");
 const productContainer = document.querySelector("#single-view");
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const productId = urlParams.get("id")
 
-fetch(`https://dummyjson.com/products/1`)
+fetch(`https://dummyjson.com/products/${myId}`)
 .then((response) => response.json())
 .then((data) => {
     console.log("Fetched data:", data);
 
     productContainer.innerHTML = `
 <div class="margin_left">
-    <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" alt="Billede">
+    <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/1.png" alt="Billede">
     <h2>${data.title}</h2>
     <h3>${data.price},-</h3>
     <p>${data.description}</p>
