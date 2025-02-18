4a116c7820393d0267e0ed8808c094956ed436aa
Annasophiathorsgaard <athorsgaard@icloud.com>
2025-02-18 13:27:03 +0100

    Added mine

--- js/product.js
+++ js/product.js
@@ -1,1 +1,26 @@
+const productContainer = document.querySelector("#single-view");
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const productId = urlParams.get("id")
+
+fetch(`https://dummyjson.com/products/1`)
+.then((response) => response.json())
+.then((data) => {
+    console.log("Fetched data:", data);
+
+    productContainer.innerHTML = `
+<div class="margin_left">
+    <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" alt="Billede">
+    <h2>${data.title}</h2>
+    <h3>${data.price},-</h3>
+    <p>${data.description}</p>
+    <div class="button"><p>Add to cart</p></div>
+</div>
+
+<div class="boks">
+  <p>${data.rating}</p>
+  <p>${data.reviewerName}</p>
+  <p>${data.comment}</p>
+</div>`
+});
