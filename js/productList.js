2bfd592821dd6b8f34b5bc3e87c649bfd2d8a9a3
MeleeseKaerhavn <125559675+MeleeseKaerhavn@users.noreply.github.com>
2025-02-20 10:07:07 +0100

    fixed link problem

--- js/productList.js
+++ js/productList.js
@@ -11,7 +11,7 @@ function showList(products) {
         markup +=
         `      
            <div class="product-card">
-                <a href="../product.html?id=${product.id}"><img src="${product.thumbnail}" alt="${product.title}"></a>
+                <a href="product.html?id=${product.id}"><img src="${product.thumbnail}" alt="${product.title}"></a>
                 <h3>${product.title}</h3>
                 <p class="price">${product.price} kr.</p>
                 <button class="buy-btn"><a href="../product.html?id=${product.id}">Read more</a></button>
